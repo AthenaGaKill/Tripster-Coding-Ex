45a1303831c2ec42218c536b16cad6e502110437
AthenaGaKill <taniaderveaux@hotmail.com>
2018-07-17 20:09:15 +0200

    little tweaks in files

--- Ex17AlphabetSoup.js
+++ Ex17AlphabetSoup.js
@@ -4,12 +4,11 @@
 function main(){
 //the user can pick a word he wants to see alphabetised
 var input = prompt("Which word would you like to see alphabetised?");
-sortInput(input);
+console.log("KAPOW! Your chosen word: " + input + " alphabetised looks like this: " + sortInput(input));
 }
 
-function sortInput(word){
-    var word = word.split('').sort().join('');
-    console.log(word);
-    return word;
+function sortInput(word){ //we use a seperate function to alphabetise the word
+    var word = word.split('').sort().join(''); //we first turn it into an array so we can use the sort method and turn it afterwards into a string again
+    return word; //word = input so technically I don't use an extra variable... I hope this is what you meant :)
 }
 main();
