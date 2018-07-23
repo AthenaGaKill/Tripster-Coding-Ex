3bdf1441d251b55116e3413eac90a9254b6e6e99
AthenaGaKill <taniaderveaux@hotmail.com>
2018-07-24 01:20:51 +0200

    back to a more basic version w print in the end...still need to test

--- Ex19bExtraEx.js
+++ Ex19bExtraEx.js
@@ -1,3 +1,4 @@
+"use strict";
 //id nr = 11nrs
 //eerste 6: jj,mm,dd
 //maand en/of dag zyn 0 indien niet gekend
@@ -13,65 +14,28 @@
 //!!!!! still need to fix the month check! Issues with figuring out how to do so because of the +20, +40...
 
 function main() {
+    //**********INPUT***********
     //user geeft zyn id nr in bestaande uit 11 nrs yymmdd-xxx xx
-    var input = prompt("Geef je rijksregisternummer in aub. Dit bestaat uit 11nrs en is terug te vinden achteraan je id kaart: ");
+    var input = prompt("Gief Id nr plz! It's the 11 nr code on your id card: ");
     while (input.length != 11) { //input needs to be 11 nrs
-        var input = prompt("Meneer kan ni tellen zeker??? Je hebt geen 11 numbers ingegeven. Probeer opnieuw!");
+        var input = prompt("Nope those are not 11 digits....Try again!");
     }
-
     var input = input.split(''); //an array makes it easier to extract nrs
-
-    console.log("The person's nr is: " + input.join(""));
     var YearOfBirth = year(input); //outputs 2 nrs = the year of birth
     var monthOfBirth = month(input); //outputs 2nrs = month of birth
-    if (monthOfBirth > 12 && genderOfPerson === true) { }
-
     var dayOfBirth = day(input); //outputs 2nrs = day of birth
-
-    var isARefugee = refugee(dayOfBirth, monthOfBirth); //checks for refugees
-    if (isARefugee === true) { //we only need output if the person is a refugee
-        console.log("This " + gender(input) + " is a refugee, and that's why the nr starts with 0000.");
-    }
-
+    var isARefugee = refugee(dayOfBirth, monthOfBirth); //checks for refugees and gives back a true or false value
     var monthFullWord = monthFullWritten(monthOfBirth, sexOfTempWorker);
-
-    if (monthOfBirth > 12) { //we only need to check for a bisnr if the monthnr > 12 (there are 12 months in a year...)
-        var sexOfTempWorker = sexTempWorker(monthOfBirth); //checks bisnr from temporary workers
-        if (sexOfTempWorker == true) {
-            console.log(" ==> This " + genderOfPerson + " was born on: " + dayOfBirth + " " + monthFullWord + " " + fullYear);
-        } else {
-            console.log("The gender of the temporary worker is unknown.");
-        }
-    }
-
     var genderOfPerson = gender(input);
-    if (sexOfTempWorker === true) { //only if we know the gender will we print it
-        console.log("Gender of the person: " + genderOfPerson);
-    }
-
-    var fullYear = millenialOrNot(input, YearOfBirth);   // will tell us when the person was born and so tell us if it is a millenial or not :)
-
-    if (dayOfBirth != 00 && monthOfBirth != 00) { //0000 = refugee so if it's not the case we can use the normal notation
-        console.log("==>  " + dayOfBirth + " " + monthFullWord + " " + fullYear);
-    }
-    switch (dayOfBirth) {
-        case 00: if (monthOfBirth != 00) { //when only the day isnt known we need a different output
-            console.log("==>  " + monthFullWord + " " + fullYear);
-        }
-        default: if (monthOfBirth == 00) {//when only the month isnt known we also need a different output
-            console.log("==>  " + dayOfBirth + " , month unknown, " + fullYear);
-        }
-            break;
-    }
-    if (dayOfBirth == 00 && monthOfBirth == 00) {
-        console.log("==> This person is a refugee born in: " + fullYear);
-    }
+    var fullYear = millenialOrNot(input, YearOfBirth);
+    //checks bisnr from temporary workers and gives back true if gender is know and false when it isnt
+    var sexOfTempWorkerKnown = sexOfTempWorkerCheck(monthOfBirth);
+    print(dayOfBirth, monthOfBirth, monthFullWord, YearOfBirth, fullYear, sexOfTempWorkerKnown, genderOfPerson, isARefugee);
 }
 
 function year(nr) { //first 2 nrs of the number = the year of birth
     var year = nr[0] + nr[1];  //concatination of 2 strings
     var yearInt = parseInt(year);
-    console.log("Year of birth: " + yearInt);
     return yearInt; //return the year so we can use it later on when we provide the info of the user
 }
 
@@ -81,7 +45,6 @@ function month(nr) {
     if (monthInt > 52) { //is the max nr that can be used taken into account the +40bisnr for temp workers from whom the gender is know / not sure how to do it for all seperate
         nr = prompt("I think you might have been mistaken. There aren't that many days in a month brah!");
     }
-    console.log("Month of birth: " + monthInt);
     return monthInt; //return the month so we can use it later on when we provide the info of the user
 }
 
@@ -91,7 +54,6 @@ function day(nr) {
     if (dayInt > 31) {
         nr = prompt("I think you might have been mistaken. There aren't that many days in a month brah!");
     }
-    console.log("Day of birth: " + dayInt);
     return dayInt; //return the day so we can use it later on when we provide the info of the user
 }
 
@@ -105,18 +67,6 @@ function refugee(birthday, birthmonth) { //check if it is a refugee or not...
     }
 }
 
-function sexTempWorker(birthmonth) { //temp workers get a bisnr. +20 when gender unknown and +40 when gender is known
-    var minTwenty = birthmonth - 20; //birthmonth +20 when gender unknown
-    var minForty = birthmonth - 40;//birthmonth +40 when gender known
-
-    if (minTwenty <= 12) { //if the birthmonth - 20 is smaller or = to 12 (12months in a year) = gender unknown
-        return false;
-    }
-    if (minForty <= 12) { //if the bm -40 is smaller or = to 12 = gender known
-        return true;
-    }
-}
-
 function gender(nr) {
     var man = "man";
     var woman = "woman";
@@ -126,6 +76,18 @@ function gender(nr) {
         return woman;
     }
     return man;
+}
+
+function sexOfTempWorkerCheck(birthmonth) { //temp workers get a bisnr. +20 when gender unknown and +40 when gender is known
+    var minTwenty = birthmonth - 20; //birthmonth +20 when gender unknown
+    var minForty = birthmonth - 40;//birthmonth +40 when gender known
+
+    if (minTwenty <= 12) { //if the birthmonth - 20 is smaller or = to 12 (12months in a year) = gender unknown
+        return false;
+    }
+    if (minForty <= 12) { //if the bm -40 is smaller or = to 12 = gender known
+        return true;
+    }
 }
 
 //personen in of na 2000 = +20000000000 before/97
@@ -163,47 +125,47 @@ function monthFullWritten(birthmonth, sexKnown) { //this turns the number indica
 
     switch (birthmonthResult) {
         case 1: monthInWord = "january";
-            return monthInWord;
             break;
         case 2: monthInWord = "february";
-            return monthInWord;
             break;
         case 3: monthInWord = "march";
-            return monthInWord;
             break;
         case 4: monthInWord = "april";
-            return monthInWord;
             break;
         case 5: monthInWord = "mai";
-            return monthInWord;
             break;
         case 6: monthInWord = "june";
-            return monthInWord;
             break;
         case 7: monthInWord = "july";
-            return monthInWord;
             break;
         case 8: monthInWord = "august";
-            return monthInWord;
             break;
         case 9: monthInWord = "september";
-            return monthInWord;
             break;
         case 10: monthInWord = "oktober";
-            return monthInWord;
             break;
         case 11: monthInWord = "november";
-            return monthInWord;
             break;
         case 12: monthInWord = "december";
-            return monthInWord;
             break;
         default: monthInWord = " "
-            return monthInWord;
     }
+    return monthInWord;
 }
+function print(dayOfBirth, monthOfBirth, monthFullWord, YearOfBirth, fullYear, sexOfTempWorkerKnown, genderOfPerson, isARefugee) {
+    console.log("Year of birth: " + YearOfBirth);
+    console.log("Month of birth: " + monthOfBirth);
+    console.log("Day of birth: " + dayOfBirth);
+    console.log("Is a refugee: " + isARefugee);
+    if (sexOfTempWorkerKnown === true)
+        console.log("Sex: " + genderOfPerson);
+    else if (sexOfTempWorkerKnown === false) {
+        console.log("Sex of the temporary worker is unknown");
+    } else {
+        console.log("Sex: " + genderOfPerson);
+    }
+    console.log("=> " + dayOfBirth + " " + monthFullWord + " " + fullYear);
 
-//MAKE A PRINT OUT FUNCTION TO PRINT OUT ALL INFO  
-
+}
 
 main();
