c30a9e42e0f28cef02730562e65ff8257e2a7349
AthenaGaKill <taniaderveaux@hotmail.com>
2018-08-04 19:35:04 +0200

    Gives same error still :( Cannot set property '0' of undefined

--- Ex22MatrixTracker.js
+++ Ex22MatrixTracker.js
@@ -1,21 +1,23 @@
 //read matrix of 16 nrs
-//store it in a multidimensial array
 //make a function that can find all positions of a certain nr + display it
 
 function main() {
-    var input = prompt("Give in your 16 nr code. We'll put it in a multi dimensional array for you: ");
-    inputArr = input.split(); //we turn the nr string input into an array
-    var multiArray = createMultiArray(inputArr);
-    console.log(multiArray);
+var input = prompt("Give in your 16nrs to have them converted into a multidimensional array:");
+var multiArrayInput = createMultiArray(input);
+console.log(multiArrayInput);
+
 }
 
 function createMultiArray(inputNr) { //we turn the input row of nrs into a multidimensional array
     var multiArr = []; // we'll add the input values into this array and turn it into a multidimensional array
-    for (var i = 0; i < inputNr.length; i++) { //loop for the rows
-        for (var j = 0; j < inputNr[i].length; j++) { //loop for the colums
-            multiArr[i][j] = inputNr[i]; //we add the individual input value into the array
+    var n = 0; //we increment this value to go through all input nrs
+    for (var i = 0; i < 4; i++) { //loop for the rows
+        for (var j = 0; j < 4; j++) { //loop for the colums
+            multiArr[i][j] = inputNr[n]; //we add the individual input value into the array
             console.log(multiArr[i][j]);
+            n++;
         }
     } return multiArr;
 }
+
 main();
