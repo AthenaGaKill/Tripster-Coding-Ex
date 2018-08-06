19489510fdfed9ce8d353cd64545378f5f85f8ef
AthenaGaKill <taniaderveaux@hotmail.com>
2018-08-06 02:59:01 +0200

    ex22 multidimensional array is created... haip!

--- Ex22MatrixTracker.js
+++ Ex22MatrixTracker.js
@@ -2,18 +2,19 @@
 //make a function that can find all positions of a certain nr + display it
 
 function main() {
-var input = prompt("Give in your 16nrs to have them converted into a multidimensional array:");
-var multiArrayInput = createMultiArray(input);
-console.log(multiArrayInput);
-
+    var input = prompt("Give in your 16nrs to have them converted into a multidimensional array:");
+    var multiArrayInput = createMultiArray(input);
+    console.log(multiArrayInput);
 }
 
 function createMultiArray(inputNr) { //we turn the input row of nrs into a multidimensional array
     var multiArr = []; // we'll add the input values into this array and turn it into a multidimensional array
     var n = 0; //we increment this value to go through all input nrs
+
     for (var i = 0; i < 4; i++) { //loop for the rows
+        multiArr[i] = []; //we define het multidim array
         for (var j = 0; j < 4; j++) { //loop for the colums
-            multiArr[i][j] = inputNr[n]; //we add the individual input value into the array
+            multiArr[i][j] = inputNr[n]; //we add the individual input value into the array (x4 first loop x 4 second loop = 16)
             console.log(multiArr[i][j]);
             n++;
         }
