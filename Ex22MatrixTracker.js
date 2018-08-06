074e6b8e58a73fcd98543f4c307596bc99350638
AthenaGaKill <taniaderveaux@hotmail.com>
2018-08-07 01:54:36 +0200

    ex22 seems to work :)

--- Ex22MatrixTracker.js
+++ Ex22MatrixTracker.js
@@ -10,34 +10,30 @@ function main() {
     console.log(multiArrayInput);
 
     var pickANr = prompt("Which nr do you wish to find the positions of?");
-    var position = findPosition(pickANr, multiArrayInput);
-    console.log(position);
+    findPosition(pickANr, multiArrayInput);
 }
 
 function findPosition(pickedNr, multiArrInput) {
-    var positions = [];
-    for (var i = 0; i < 4; i++) { //loop for the rows     
-        positions[i] = [];
+    //we need to check each element and compare it to the nr we are looking for
+    for (var i = 0; i < 4; i++) { //loop for the rows
         for (var j = 0; j < 4; j++) { //loop for the colums
-            if (multiArrInput[i][j] == pickedNr) {
-                // positions += multiArrInput[i][j].indexOf(pickedNr);
-                positions[positions.length] = multiArrInput[i][j];
-                console.log((multiArrInput[i][j]));
+            if (multiArrInput[i][j] == pickedNr) {  //if it is the same we need to write down the index               
+                console.log("Your picked nr can be found at the following index: " + i + " " + j);
             }
         }
-    } console.log(positions);
-    return positions;
+    }
 }
 
 function createMultiArray(inputNr) { //we turn the input row of nrs into a multidimensional array
     var multiArr = []; // we'll add the input values into this array and turn it into a multidimensional array
     var n = 0; //we increment this value to go through all input nrs
 
+
     for (var i = 0; i < 4; i++) { //loop for the rows
         multiArr[i] = []; //we define het multidim array
         for (var j = 0; j < 4; j++) { //loop for the colums
             multiArr[i][j] = inputNr[n]; //we add the individual input value into the array (x4 first loop x 4 second loop = 16)
-            console.log(multiArr[i][j]);
+            // console.log(multiArr[i][j]);
             n++;
         }
     } return multiArr;
